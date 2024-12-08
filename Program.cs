5cdd6b99c72aafbdec1cb74816e4b043585f276b
PASSWOED <PASSWOED@DESKTOP-UI80RJC>
2024-12-08 17:55:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CheckifSquare.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region CheckifSquare
+            Console.WriteLine("Enter The Width : ");
+            Double Width=Convert.ToDouble (Console.ReadLine());
+            Console.WriteLine("Enter The Length : ");
+            Double Length = Convert.ToDouble(Console.ReadLine());
+            if (Width == Length)
+            {
+                Console.WriteLine("This is a Squre");
+            }
+            else
+            {
+                Console.WriteLine("This is not a Squre");
+
+            }
+            #endregion
+        }
+    }
+}
